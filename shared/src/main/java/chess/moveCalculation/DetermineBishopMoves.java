6bef7ccffe5bb7ea91b5fc881ca56588aaa28fc9
Ruth Thomas <ruthathomas026@gmail.com>
2024-09-11 23:45:22 -0600

    Created moveCalculation subpackage, DeterminePieceMoves interface, and DetermineBishopMoves subclass

---
+++ shared/src/main/java/chess/moveCalculation/DetermineBishopMoves.java
@@ -0,0 +1,5 @@
+package chess.moveCalculation;
+
+public class DetermineBishopMoves implements DeterminePieceMoves {
+    //FIXME: >:) implement the methods from DeterminePieceMoves here
+}
---
+++ shared/src/main/java/chess/moveCalculation/DeterminePieceMoves.java
@@ -0,0 +1,6 @@
+package chess.moveCalculation;
+
+public interface DeterminePieceMoves {
+    //FIXME: I don't know anything about interfaces yet. but I will
+    //FIXME: Determine/add the methods for the subclasses to implement
+}
