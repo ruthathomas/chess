03a75a74b9af85e0672af3ff8725a782b751b365
Ruth Thomas <ruthathomas026@gmail.com>
2024-09-20 20:31:18 -0600

    Updated ChessBoard (resetBoard func, spec) to pass all ChessBoardTests

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -66,7 +66,34 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        for(var color : ChessGame.TeamColor.values()) {
+            int firstRow = 0;
+            int secondRow = 0;
+            if(color == ChessGame.TeamColor.WHITE) {
+                firstRow = 1;
+                secondRow = 2;
+            } else {
+                firstRow = 8;
+                secondRow = 7;
+            }
+            // rooks
+            addPiece(new ChessPosition(firstRow, 1), new ChessPiece(color, ChessPiece.PieceType.ROOK));
+            addPiece(new ChessPosition(firstRow, 8), new ChessPiece(color, ChessPiece.PieceType.ROOK));
+            // knights
+            addPiece(new ChessPosition(firstRow, 2), new ChessPiece(color, ChessPiece.PieceType.KNIGHT));
+            addPiece(new ChessPosition(firstRow, 7), new ChessPiece(color, ChessPiece.PieceType.KNIGHT));
+            // bishops
+            addPiece(new ChessPosition(firstRow, 3), new ChessPiece(color, ChessPiece.PieceType.BISHOP));
+            addPiece(new ChessPosition(firstRow, 6), new ChessPiece(color, ChessPiece.PieceType.BISHOP));
+            // queen
+            addPiece(new ChessPosition(firstRow, 4), new ChessPiece(color, ChessPiece.PieceType.QUEEN));
+            // king
+            addPiece(new ChessPosition(firstRow, 5), new ChessPiece(color, ChessPiece.PieceType.KING));
+            // pawns
+            for(int i = 1; i < 9; i++) {
+                addPiece(new ChessPosition(secondRow, i), new ChessPiece(color, ChessPiece.PieceType.PAWN));
+            }
+        }
     }
 
     /**
