188622605504635ce76a6eb161038a1379950386
Ruth Thomas <ruthathomas026@gmail.com>
2024-10-19 21:35:28 -0600

    I changed my mind and wanted only one interface for DataAccess, so I undid everything and went back to that.

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -3,7 +3,19 @@ package service;
 import model.AuthData;
 
 public class AuthService {
+
+    // DataAccess object; once database is all squared away, this can be changed
+
+
+    public void clearAuthData() {
+        //
+    }
+
+    public void delAuthData(AuthData auth) {
+        //FIXME
+    }
    // ALSO in the works
 //    public void createAuth(AuthData auth) {}
 //    private void delAuth(AuthData auth) {}
+
 }
