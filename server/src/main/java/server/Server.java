0b26fd06d4b053b12c99371c4d2271a0d81ed35f
Ruth Thomas <ruthathomas026@gmail.com>
2024-10-22 11:47:08 -0600

    Added clear and register endpoints.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
+import com.google.gson.Gson;
 import dataaccess.MemoryDataAccess;
+import model.UserData;
 import org.eclipse.jetty.server.Authentication;
 import service.AuthService;
 import service.GameService;
@@ -9,10 +11,13 @@ import spark.*;
 
 public class Server {
 
+    //FIXME this feels more coupled than I would like it to be. Server layer shouldn't
+    // know about Data Layer. I want to come back later and rework, if possible.
     private MemoryDataAccess localMemory = new MemoryDataAccess();
     private AuthService authService = new AuthService(localMemory);
     private GameService gameService = new GameService(localMemory);
     private UserService userService = new UserService(localMemory);
+    private Gson serializer = new Gson();
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -21,9 +26,9 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
         // Clear Endpoint
-        // Spark.delete("/db", "FIXME");
+        Spark.delete("/db", this::clear);
         // Register User Endpoint
-        // Spark.post("/user", "FIXME");
+        Spark.post("/user", this::registerUser);
         // Login Endpoint
         // Spark.post("/session", "FIXME");
         // Logout Endpoint
@@ -45,5 +50,41 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private void exceptionHandler(ResponseException ex, Request req, Response res) {
+        //FIXME
+        System.out.println("Exception caught; fixme");
+    }
+    //throw exceptions on these?
+
+    //fixme these throw exceptions in the example so probably I should do that
+    private Object registerUser(Request req, Response res) {
+        var newUser = serializer.fromJson(req.body(), UserData.class);
+        var result = userService.register(newUser);
+        return serializer.toJson(result);
+    }
+
+    private Object loginUser(Request req, Response res) {
+
+        return null;
+    }
+
+    private Object logoutUser(Request req, Response res) {
+
+        return null;
+    }
+
+    private Object joinGame(Request req, Response res) {
+
+        return null;
+    }
+
+    private Object clear(Request req, Response res) throws ResponseException {
+        authService.clearData();
+        gameService.clearData();
+        userService.clearData();
+        res.status(200);
+        return "";
     }
 }
