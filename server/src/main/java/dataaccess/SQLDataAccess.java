c5e26b35cc304445659c99ab4b55b6cc629fe299
Ruth Thomas <ruthathomas026@gmail.com>
2024-10-29 21:00:49 -0600

    Began implementing SQLDataAccess methods.

--- server/src/main/java/dataaccess/SQLDataAccess.java
+++ server/src/main/java/dataaccess/SQLDataAccess.java
@@ -4,7 +4,10 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import java.sql.SQLException;
 import java.util.Map;
+
+import static dataaccess.DatabaseManager.getConnection;
 
 public class SQLDataAccess implements DataAccessInterface {
 
@@ -40,11 +43,18 @@ public class SQLDataAccess implements DataAccessInterface {
 
     @Override
     public void addGame(GameData gameData) {
+        // 1. You're going to have to serialize the chess game to a JSON string
+        // 2. and then you're going to have to put it in that database
         //
     }
 
     @Override
     public GameData updateGame(int gameID, GameData gameData) throws DataAccessException {
+//        1. Select the game’s state (JSON string) from the database
+//        2. Deserialize the JSON string to a ChessGame Java object
+//        3. Update the state of the ChessGame object
+//        4. Re-serialize the Chess game to a JSON string
+//        5. Update the game’s JSON string in the database
         return null;
     }
 
@@ -65,6 +75,36 @@ public class SQLDataAccess implements DataAccessInterface {
 
     @Override
     public void clearUserData() {
+
+    }
+
+    private Object selectObject(String workingVar) {
+        //FIXME I'm not sure what I'm doing
+        try(var conn = getConnection()) {
+            try(var preparedStatement = conn.prepareStatement("SELECT ?")) {
+                preparedStatement.setString(1, workingVar);
+
+                var rs = preparedStatement.executeQuery();
+                rs.next();
+                rs.getString(1);
+            }
+        } catch (DataAccessException | SQLException e) {
+            //do something
+        }
+        return null;
+
+//        private static void simpleParameter(int a, int b) throws SQLException {
+//            try (var conn = getConnection()) {
+//                try (var preparedStatement = conn.prepareStatement("SELECT ?+?")) {
+//                    preparedStatement.setInt(1, a);
+//                    preparedStatement.setInt(2, b);
+//
+//                    var rs = preparedStatement.executeQuery();
+//                    rs.next();
+//                    System.out.println(rs.getInt(1));
+//                }
+//            }
+//        }
 
     }
 
